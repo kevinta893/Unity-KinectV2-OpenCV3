3164c3416eb9ad9071d9408ae9f528b585d15e9f
kevinta893 <kevinta893@gmail.com>
2018-03-16 22:41:25 -0600

    Change cache to download
    Unity now needs to be installed from the cache

--- travis-build/install-unity.sh
+++ travis-build/install-unity.sh
@@ -1,38 +1,48 @@
 #! /bin/sh
 
-BASE_URL=https://beta.unity3d.com/download
-HASH=46dda1414e51
-VERSION=2017.2.0f3
-
-download() {
-  file=$1
-  url="$BASE_URL/$HASH/$package"
-
-  echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
-}
-
-install() {
-  package=$1
-  download "$package"
-
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
-}
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
+UNITY_DOWNLOAD_CACHE=$(pwd)/unity_download_cache
+BASE_URL=https://beta.unity3d.com/download/46dda1414e51
+VERSION=2017.2.0f3
 
+UNITY_OSX_PACKAGE="MacEditorInstaller/Unity-$VERSION.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE="MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
 
-FILE=""
-DIR="/Applications/Unity/"
-# init
-# look for empty dir 
-if [ "$(ls -A $DIR)" ]; then
-     echo "Unity Exists. Will not redownload and install. Clear cache for clean install"
+# Check if unity was already downloaded
+if [ ! "$(ls -A $UNITY_DOWNLOAD_CACHE)" ]; then
+	echo "Unity does not exist. Download and installing:"
+	download "$UNITY_OSX_PACKAGE"
+	download "$UNITY_WINDOWS_TARGET_PACKAGE"
 else
-    echo "Unity does not exist. Download and installing:"
-	install "MacEditorInstaller/Unity-$VERSION.pkg"
-	install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+	echo "Unity Exists. Will not redownload. Proceeding to install from cache..."
 fi
+
+echo "Installing Unity..."
+install "$UNITY_OSX_PACKAGE"
+install "$UNITY_WINDOWS_TARGET_PACKAGE"
+
+
+
+download() {
+	
+	mkdir "$UNITY_DOWNLOAD_CACHE"
+	file=$1
+	url="$BASE_URL/$file"
+
+	echo "Downloading from $url: "
+	curl -o `basename "$package"` "$url"
+}
+
+install() {
+	package=$1
+
+	echo "Installing "`basename "$package"`
+	sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+
+
+
 
